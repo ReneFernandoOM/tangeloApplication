e2dfa282e8611385c5c53d488829551755bd005a
Rene <reortega@spsolutions.com.mx>
2022-06-09 18:14:52 -0600

    Se agrega docstrings y comentarios

--- src/run.py
+++ src/run.py
@@ -6,8 +6,11 @@ import datetime
 from utils import get_all_countries, get_country_info, encrypt_string
 
 
-
 def main():
+    """
+    Función que se encarga de procesar los países, obteniendo su nombre, región, 
+    lenguaje encriptado y el tiempo que se tardó en la obtención de estas variables.
+    """
     countries = get_all_countries()
     data = []
     for country in countries:
@@ -19,10 +22,10 @@ def main():
             continue
         country_name, country_region, country_languages = country_info
         
+        # need it in case there is more than one language in a country
         middle_time = datetime.datetime.now() - start_time
         language_time = datetime.timedelta(0)
         for language in country_languages:
-            
             sha1_encrypted_languaje = encrypt_string(language)
             end_time = datetime.datetime.now()
 
@@ -30,6 +33,8 @@ def main():
             if language_time == 0:
                 language_time = final_time
             else:
+                # in case the country has more than one language,
+                # add the time it took before reaching the loop
                 final_time + middle_time
 
             final_time = final_time.total_seconds() * 1000
--- src/utils.py
+++ src/utils.py
@@ -6,6 +6,10 @@ from config import API_URL, API_RESOURCE
 from errors import ExpectedError
 
 def get_all_countries() -> List:
+    """
+    Función que se encarga de retornar todos los países de la 
+    respuesta de la API
+    """
     api_response = requests.get(f"{API_URL}/{API_RESOURCE}")
 
     if api_response.ok:
@@ -15,6 +19,15 @@ def get_all_countries() -> List:
     raise ExpectedError('API is not responding as expected')
 
 def get_country_info(country: Dict) -> Tuple[str, str, List]:
+    """
+    Se encarga de obtener la información necesaria del país.
+    Args:
+        - conutry: Dict | Diccionario con la información del País
+    Returns:
+        - country_name: Str | Nombre del país
+        - country_region: Str | Región a la que pertenece el país
+        - languages: List | Lista de los lenguajes del país
+    """
     country_name_info = country.get('name', {})
     country_name = country_name_info.get('common')
     
@@ -29,6 +42,13 @@ def get_country_info(country: Dict) -> Tuple[str, str, List]:
         return country_name, region, list(languages.values())
 
 def encrypt_string(string: str) -> str:
+    """
+    Función que se encarga de encriptar el string
+    Args:
+        - string: Str | String que se va a encriptar
+    Returns:
+        - encrypted_string: Str | String encriptada
+    """
     encrypted_string = hashlib.sha1(string.encode()).hexdigest()
     return encrypted_string
 
